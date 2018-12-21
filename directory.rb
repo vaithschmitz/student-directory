50531dbca49844bec6d69af60e78190dad726edd
vaithschmitz <vaithschmitz@googlemail.com>
2018-12-21 15:38:25 +0000

    added choose by letter

--- directory.rb
+++ directory.rb
@@ -1,24 +1,43 @@
-students = [
-{name: "Dr. Hannibal Lecter", cohort: :november},
-{name: "Darth Vader", cohort: :november},
-{name: "Nurse Ratched", cohort: :november},
-{name: "Michael Corleone", cohort: :november},
-{name: "Alex DeLarge", cohort: :november},
-{name: "The Wicked Witch of the West", cohort: :november},
-{name: "Terminator", cohort: :november},
-{name: "Freddy Krueger", cohort: :november},
-{name: "The Joker", cohort: :november},
-{name: "Joffrey Baratheon", cohort: :november},
-{name: "Norman Bates", cohort: :november},
-]
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = Array.new
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do 
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+
+students = input_students
 
 def print_header 
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
-def print_names(students)
-  students.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)" }
+def print(students)
+  puts "Do you want to see all students or only those starting with a specific letter?"
+  answer = gets.chomp
+
+  if answer != "all"
+  	puts "What's the first letter of students you want to see?"
+    first_letter = gets.chomp
+    students.map { |student| if student[:name].split("")[0] == first_letter then puts "#{student[:name]} is in the #{student[:cohort]} cohort" end}
+
+  else
+    students.each_with_index { |student, index| 
+    puts "#{index+1}. #{student[:name]} is in the #{student[:cohort]} cohort!"}
+
+  end
 end
 
 def print_footer(students) 
@@ -26,5 +45,5 @@ def print_footer(students)
 end
 
 print_header
-print_names(students)
+print(students)
 print_footer(students)
