c508e306d00bf955f43a05840c09b8b199f68baf
vaithschmitz <vaithschmitz@googlemail.com>
2019-01-02 17:29:32 +0000

    sort by cohort

--- directory.rb
+++ directory.rb
@@ -1,23 +1,36 @@
-$cohorts_list = [:January, :February, :March, :April, :May, :June, :July, :August, :September, :October, :November, :December]
-
 def input_students
   puts "Please enter the names of the students"
   # create an empty array
-  students = Array.new
+  $students = []
+
   # get the first name
   name = gets.chomp.capitalize
+
   # while the name is not empty, repeat this code
   puts "What cohort will #{name} be in?"
+
+  # ask for cohort, turn to sym 
   cohort = gets.chomp.capitalize.to_sym
+    # if input empty > cohort TBD
     if cohort.empty? then cohort = :TBD end
-    if $cohorts_list.include?(cohort) == false then  puts "Choose one of the following: #{$cohorts_list}"; cohort = gets.chomp.to_sym end
+  
+  # ask for food
   puts "What's #{name}'s favorite food?"
   food = gets.chomp.capitalize
+  
   while !name.empty? do 
+
     # add the student hash to the array
-    students << {name: name, food: food, cohort: cohort}
-    puts "Now we have #{students.count} students"
+    $students << {name: name, cohort: cohort.to_sym, food: food}
+    
+    if $students.length == 1
+      puts "Now we have 1 student"
+    else
+      puts "Now we have #{$students.length} students"
+    end
+
     puts "Who else is in the cohort?"
+    
     name = gets.chomp.capitalize
       # break loop if no input
       if name.empty? then break end
@@ -29,10 +42,9 @@ def input_students
     end
   
   # return the array of students
-  return students
+  return $students
 end
 
-students = input_students
 
 def print_header 
   puts "The students of Villains Academy"
@@ -40,44 +52,27 @@ def print_header
 end
 
 def print(students)
+
+  # create new array for output sorted by cohort
+  cohort_list = Array.new
   puts "Which cohort do you want to see?"
-  answer_cohort = gets.chomp
+  answer_cohort = gets.chomp.capitalize.to_sym
   
+  $students.map{ |x| if x[:cohort] == answer_cohort then cohort_list << x end}
+  cohort_list.map {|student| puts "#{student[:name]} loves #{student[:food]}"}
 
   puts "Do you want to see all students or only those starting with a specific letter? (Please enter all for all)"
   answer_letter = gets.chomp
 
   if answer_letter != "all"
     puts "What's the first letter of students you want to see?"
-    first_letter = gets.chomp
+    first_letter = gets.chomp.capitalize
     # make global var to store students with letter based on user_input
-    $letter_students = []
-    students.map { |student| if student[:name].split("")[0] == first_letter then $letter_students << student end}
+    letter_students = []
+    cohort_list.map { |student| if student[:name].split("")[0] == first_letter then letter_students << student end}
+    puts letter_students.map {|student| puts "#{student[:name]} loves #{student[:food]}"}
   end
 
-  puts "Do you want to see all students or only short names? (Please enter all for all)"
-  answer_long = gets.chomp
-
-  # print only students with first letter based on user input
-  if answer_letter != "all" && answer_long == "all"
-    $letter_students.map { |student| puts "#{student[:name]} loves #{student[:food]} and is in the #{student[:cohort]} cohort!"} 
-  
-  # print all names but only if chars < 12
-  elsif answer_letter == "all" && answer_long != "all" 
-    students.map { |student| if student[:name].split("").length < 12 then puts "#{student[:name]} loves #{student[:food]} and is in the #{student[:cohort]} cohort" end}
-
-  # print only user-inputted first letter names with chars < 12
-  elsif 
-    answer_letter != "all" && answer_long != "all"
-    $letter_students.map { |student| if student[:name].split("").length < 12 then puts "#{student[:name]} loves #{student[:food]} and is in the #{student[:cohort]} cohort" end}
-
-  # print all students regardless
-  else
-    students.each_with_index { |student, index| 
-    puts "#{index+1}. #{student[:name]} loves #{student[:food]} and
-     is in the #{student[:cohort]} cohort!"}
-
-  end
 end
 
 def print_footer(students) 
@@ -88,6 +83,7 @@ def print_footer(students)
   end
 end
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
